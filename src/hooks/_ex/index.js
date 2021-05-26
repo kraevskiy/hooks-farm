c3c759548882e0b3b2473b1a9cdbb2cb78f02bbc
kraevskiy <rambutan@educonlines.com>
2021-05-26 10:03:21 +0300

    .

---
+++ src/hooks/_ex/index.js
@@ -0,0 +1,11 @@
+
+const Ch_11 = () => {
+  return (
+    <>
+      <h2>useInterval</h2>
+
+    </>
+  )
+}
+
+export default Ch_11
---
+++ src/hooks/_ex/useInterval.js
@@ -0,0 +1,8 @@
+
+
+export function useInterval() {
+
+
+  return null
+}
+
